e799c0389c6473cd6a6c0296ba64392db71a326a
Archit Batra <rapchit93@gmail.com>
2018-03-03 14:44:50 +0530

    added all project files

---
+++ src/lightapp1/HttpRequestADN15.java
@@ -0,0 +1,92 @@
+package lightapp1;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class HttpRequestADN15 {
+
+	private final String USER_AGENT = "Mozilla/5.0";
+
+	public static void main(String[] args) throws Exception {
+
+		HttpRequestADN15 http = new HttpRequestADN15();
+
+		//System.out.println("Testing 1 - Send Http GET request");
+		//http.sendGet();
+
+		System.out.println("\nTesting 2 - Send Http POST request");
+		http.sendPost();
+
+	}
+	
+	// HTTP GET request
+	/*private void sendGet() throws Exception {
+
+		String url = "http://www.google.com/search?q=mkyong";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		//con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}*/
+
+	// HTTP POST request
+	private void sendPost() throws Exception {
+
+		String url = "http://192.168.1.252:8080/R0?fu=1&ty=2&drt=2&lbl=Narn-AB_dc";
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		//add reuqest header
+		con.setRequestMethod("GET");
+		con.setRequestProperty("X-M2M-Origin","S27");
+//		con.setRequestProperty("Content-Type", "application/json;ty=2");
+		con.setRequestProperty("X-M2M-RI","nar");
+		con.setRequestProperty("Accept", "application/json");
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
+}
---
+++ src/lightapp1/HttpRequestADN6.java
@@ -0,0 +1,103 @@
+package lightapp1;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class HttpRequestADN6 {
+
+	private final String USER_AGENT = "Mozilla/5.0";
+
+	public static void main(String[] args) throws Exception {
+
+		HttpRequestADN6 http = new HttpRequestADN6();
+
+		//System.out.println("Testing 1 - Send Http GET request");
+		//http.sendGet();
+
+		System.out.println("\nTesting 2 - Send Http POST request");
+		http.sendPost();
+
+	}
+	
+	// HTTP GET request
+	private void sendGet() throws Exception {
+
+		String url = "http://www.google.com/search?q=mkyong";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		//con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
+	// HTTP POST request
+	private void sendPost() throws Exception {
+
+		String url = "http://192.168.1.252:8080/R79";
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		//add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("X-M2M-Origin","S27");
+		con.setRequestProperty("Content-Type", "application/json;ty=3");
+		con.setRequestProperty("X-M2M-RI","nar");
+		con.setRequestProperty("Accept", "application/json");
+
+		//payload
+		String payload = "{\"m2m:cnt\":{\"lbl\":[\"Narn-AB_state\",\"state\"],\"rn\":\"cnt_Narn-AB_state2\"}}";
+
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(payload);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + payload);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
+}
